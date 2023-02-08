eae00c94b4fc455581034e2d6bdefb4c9c025949
Anson Ng <914anson5@gmail.com>
2023-02-08 23:14:03 +0800

    style: reformat all source code files
    Closed KL-18

--- src/main/kotlin/com/example/model/Submissions.kt
+++ src/main/kotlin/com/example/model/Submissions.kt
@@ -2,7 +2,7 @@ package com.example.model
 
 import org.jetbrains.exposed.sql.Table
 
-object Submissions: Table() {
+object Submissions : Table() {
     val id = integer("SubmissionId").autoIncrement()
     val language = varchar("Language", 255)
     val code = text("Code")
